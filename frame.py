5c47bc1aa41dcbb87f3855a2c80cbd07d88268d9
Sehajdeep Singh <55930236+sehajdeep-singh@users.noreply.github.com>
2020-10-28 20:47:13 -0400

    Update frame.py
    Function added to take a color road frame as input and infer the lane lines in the image

--- frame.py
+++ frame.py
@@ -1,3 +1,56 @@
+def get_lane_lines(color_image, solid_lines=True):
+    """
+    This function take as input a color road frame and tries to infer the lane lines in the image.
+    :param color_image: input frame
+    :param solid_lines: if True, only selected lane lines are returned. If False, all candidate lines are returned.
+    :return: list of (candidate) lane lines.
+    """
+    # resize to 960 x 540
+    color_image = cv2.resize(color_image, (960, 540))
+
+    # convert to grayscale
+    img_gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
+    # perform gaussian blur
+    img_blur = cv2.GaussianBlur(img_gray, (17, 17), 0) #edge detection is susceptible to noise in the image.
+                                                       #Hence performing gaussian blur to remove noise
+
+    # perform edge detection
+    img_edge = cv2.Canny(img_blur, threshold1=50, threshold2=80) #edge detection using Canny algorithm
+
+    # Temp edge detection output
+
+    global counter
+    out_path = join('temp', 'edge_detected_images', str(counter)+".jpg")
+    cv2.imwrite(out_path, cv2.cvtColor(img_edge, cv2.COLOR_RGB2BGR))
+    counter = counter + 1
+
+    # perform hough transform
+    detected_lines = hough_lines_detection(img=img_edge,
+                                           rho=2,
+                                           theta=np.pi / 180,
+                                           threshold=1,
+                                           min_line_len=15,
+                                           max_line_gap=5)
+
+    # convert (x1, y1, x2, y2) tuples into Lines
+    detected_lines = [Line(l[0][0], l[0][1], l[0][2], l[0][3]) for l in detected_lines]
+
+    # if 'solid_lines' infer the two lane lines
+    if solid_lines:
+        candidate_lines = []
+        for line in detected_lines:
+                # consider only lines with slope between 30 and 60 degrees
+                if 0.5 <= np.abs(line.slope) <= 2:
+                    candidate_lines.append(line)
+        # interpolate lines candidates to find both lanes0 
+        lane_lines = compute_lane_from_candidates(candidate_lines, img_gray.shape)
+    else:
+        # if not solid_lines, just return the hough transform output
+        lane_lines = detected_lines
+
+    return lane_lines
+
 def color_frame_pipeline(frames, solid_lines=True, temporal_smoothing=True):
     
     is_videoclip = len(frames) > 0
